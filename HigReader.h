87b934a5a769deb9f77e899cb07709805f556eb3
Abraham Amador <aamador994@gmail.com>
2020-10-18 20:22:04 -0500

    Update HigReader.h

--- HigReader.h
+++ HigReader.h
@@ -3,9 +3,13 @@
 
 char FileName[100];
 FILE *CurrentFile;
+bool IsEnglish;
+char identifier[19];
 
 void GetFileName();
 bool ReadFile();
 void LineValidation(char *currentLine);
+void CharacterValidation(char *currentLine);
 
 #endif
+
